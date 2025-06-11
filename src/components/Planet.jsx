9074237671abe05731efc960a019a5cf81fb116d
deepesh-sr <220714100078@cutm.ac.in>
2025-06-11 19:36:21 +0530

    texture field added :)

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,11 +1,16 @@
-const Planet = (props) => {
+import { useTexture } from "@react-three/drei";
+import { PLANET_DATA } from "../constants/Index";
+const Planet = ({planetData ,props}) => {
+
+    const texture = useTexture(planetData)
     return (
         <mesh
             {...props}
         >
             <sphereGeometry args={[1, 32, 32]} />
             <meshStandardMaterial
-                color={props.color}
+                map={texture}
+                // color={props.color}
                 // emissive={'#1C6FA0'}
                 emissiveIntensity={0.5}
                 roughness={1}
