b76c6d0c283f61ae646bc9dc673cf436b69fda99
deepesh-sr <220714100078@cutm.ac.in>
2025-06-12 00:23:39 +0530

    Planet texture added

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,20 +1,18 @@
 import { useTexture } from "@react-three/drei";
-import { PLANET_DATA } from "../constants/Index";
-const Planet = ({planetData ,props}) => {
+const Planet = ({texturePath,...props}) => {
 
-    const texture = useTexture(planetData)
+    const texture = useTexture(texturePath)
     return (
         <mesh
             {...props}
         >
             <sphereGeometry args={[1, 32, 32]} />
             <meshStandardMaterial
-                map={texture}
-                // color={props.color}
                 // emissive={'#1C6FA0'}
                 emissiveIntensity={0.5}
                 roughness={1}
-                metalness={0} />
+                metalness={0}
+                map={texture} />
         </mesh>
     )
 }
